a6bf14e8dd0095352e6dce69aa16a3b79d6fd7ab
Soyeon Bak <bluewaters1104@gmail.com>
2023-07-01 15:30:11 +0900

    day 4 - 파일이동

--- test_file/data_set.py
+++ test_file/data_set.py
@@ -9,8 +9,8 @@ from nltk.stem.snowball import SnowballStemmer
 # 영화 추천 시스템 만들기(단순버전, 평점기준 - 알고리즘이 들어가지 않는)
 
 # 자료 읽어오기
-md2 = pd.read_csv('tmdb_5000_credits.csv')
-md = pd.read_csv('tmdb_5000_movies.csv')
+md2 = pd.read_csv('../tmdb_5000_credits.csv')
+md = pd.read_csv('../tmdb_5000_movies.csv')
 md2.columns = ['id','tittle','cast','crew']
 md= md.merge(md2,on='id')
 print(md2.columns)
@@ -119,7 +119,7 @@ unique_values = md['genres'].unique()
 
 # 내용 기반으로 영화 추천하기 - 영화의 속성값 저장
 # TF-IDF를 연산할 때 데이터에 Null 값이 들어가면 에러가 발생한다. 결측값에 해당하는 Null 값 확인하고 지워줌
-id_not_null_df = pd.read_csv('tmdb_5000_movies.csv')
+id_not_null_df = pd.read_csv('../tmdb_5000_movies.csv')
 con_id_not_null = id_not_null_df['id'].notnull()
 id_not_null_df = id_not_null_df[con_id_not_null]['id'].astype('int')
 # md = md.drop([[19730, 19530, 35587]])
--- test_file/loading_test.py
+++ test_file/loading_test.py
@@ -10,7 +10,7 @@ def resource_path(relative_path):
     base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
-loading_form = resource_path('loading_page.ui')
+loading_form = resource_path('../loading_page.ui')
 loading_ui = uic.loadUiType(loading_form)[0]
 
 class Loading(QDialog, loading_ui):
@@ -18,8 +18,8 @@ class Loading(QDialog, loading_ui):
         super().__init__()
         self.setupUi(self)
 
-        movie = QMovie('./img/search.gif')
-        self.loading_img.setPixmap(QPixmap('./img/search.gif'))
+        movie = QMovie('../img/search.gif')
+        self.loading_img.setPixmap(QPixmap('../img/search.gif'))
         self.loading_img.setMovie(movie)
         movie.start()
 
